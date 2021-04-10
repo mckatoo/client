4c8d1311bd4696fd109f2837adcab3807feb474f
Milton Carlos Katoo <mckatoo@gmail.com>
2021-04-09 21:59:06 -0300

    feat(formsignin): sign-in with next-auth

--- src/components/FormSignIn/test.tsx
+++ src/components/FormSignIn/test.tsx
@@ -1,10 +1,26 @@
+import { MockedProvider } from '@apollo/client/testing'
 import { render, screen } from 'utils/test-utils'
 
 import FormSignIn from '.'
 
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const useRouter = jest.spyOn(require('next/router'), 'useRouter')
+const push = jest.fn()
+
+useRouter.mockImplementation(() => ({
+  push,
+  query: '',
+  asPath: '',
+  route: '/'
+}))
+
 describe('<FormSignIn />', () => {
   it('should render the form', () => {
-    const { container } = render(<FormSignIn />)
+    const { container } = render(
+      <MockedProvider>
+        <FormSignIn />
+      </MockedProvider>
+    )
 
     expect(screen.getByPlaceholderText(/email/i)).toBeInTheDocument()
     expect(screen.getByPlaceholderText(/password/i)).toBeInTheDocument()
@@ -16,7 +32,11 @@ describe('<FormSignIn />', () => {
   })
 
   it('should render the forgot password link', () => {
-    render(<FormSignIn />)
+    render(
+      <MockedProvider>
+        <FormSignIn />
+      </MockedProvider>
+    )
 
     expect(
       screen.getByRole('link', { name: /forgot your password\?/i })
@@ -24,7 +44,11 @@ describe('<FormSignIn />', () => {
   })
 
   it('should render text to sign up if already have an account', () => {
-    render(<FormSignIn />)
+    render(
+      <MockedProvider>
+        <FormSignIn />
+      </MockedProvider>
+    )
 
     expect(screen.getByRole('link', { name: /sign up/i })).toBeInTheDocument()
     expect(screen.getByText(/don’t have an account\?/i)).toBeInTheDocument()
