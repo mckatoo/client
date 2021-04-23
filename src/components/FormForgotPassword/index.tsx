4526b62013fc7d55ea584d0d02cd68f1c9aba215
Milton Carlos Katoo <mckatoo@gmail.com>
2021-04-23 14:45:42 -0300

    refactor(formforgotpassword): switch fields password and new passwords for reset link

--- src/components/FormForgotPassword/index.tsx
+++ src/components/FormForgotPassword/index.tsx
@@ -11,14 +11,16 @@ import {
   FormWrapper
 } from 'components/Form'
 import TextField from 'components/TextField'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 import { FieldErrors, forgotValidate } from 'utils/validations'
 
 const FormForgotPassword = () => {
+  const { query } = useRouter()
   const [success, setSuccess] = useState(false)
   const [formError, setFormError] = useState('')
   const [fieldError, setFieldError] = useState<FieldErrors>({})
-  const [values, setValues] = useState({ email: '' })
+  const [values, setValues] = useState({ email: (query.email as string) || '' })
   const [loading, setLoading] = useState(false)
 
   const handleInput = (field: string, value: string) => {
@@ -82,6 +84,7 @@ const FormForgotPassword = () => {
               placeholder="Email"
               type="text"
               error={fieldError?.email}
+              initialValue={query.email as string}
               onInputChange={(v) => handleInput('email', v)}
               icon={<Email />}
             />
