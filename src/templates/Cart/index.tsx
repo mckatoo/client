aa3685690ebc558b224d9c7398f2d6061bd412b1
Milton Carlos Katoo <mckatoo@gmail.com>
2021-05-07 15:17:25 -0300

    init integration with stripe

--- src/templates/Cart/index.tsx
+++ src/templates/Cart/index.tsx
@@ -8,6 +8,8 @@ import PaymentForm from 'components/PaymentForm'
 import Showcase from 'components/Showcase'
 import Base from 'templates/Base'
 
+import { Elements } from '@stripe/react-stripe-js'
+import { loadStripe } from '@stripe/stripe-js'
 import { Info } from '@styled-icons/material-outlined/Info'
 
 import * as S from './styles'
@@ -16,47 +18,45 @@ export type CartProps = {
   recommendedTitle: string
   recommendedGames: GameCardProps[]
   recommendedHighlight: HighlightProps
-} & CartListProps &
-  Pick<PaymentOptionsProps, 'cards'>
+} & CartListProps
+
+const stripe = loadStripe(`${process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY}`)
 
 const Cart = ({
   recommendedTitle,
   recommendedGames,
-  recommendedHighlight,
-  cards
-}: CartProps) => {
-  const handlePayment = () => ({})
+  recommendedHighlight
+}: CartProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor='secondary'>
+        My cart
+      </Heading>
 
-  return (
-    <Base>
-      <Container>
-        <Heading lineLeft lineColor="secondary">
-          My cart
-        </Heading>
+      <S.Content>
+        <CartList />
 
-        <S.Content>
-          <CartList />
+        <Elements stripe={stripe}>
+          <PaymentForm />
+        </Elements>
+      </S.Content>
+      <S.Text>
+        <Info size={18} /> Your purchase is protected by a secure connection
+        from the WON platform. By purchasing from our store you agree and agree
+        to our <a href='#'>terms of use.</a> After making the purchase you are
+        entitled to a refund within a maximum of 30 days, without any additional
+        cost, as long as the download of the purchased game has not occurred
+        after your purchase.
+      </S.Text>
+      <Divider />
+    </Container>
 
-          <PaymentOptions cards={cards} handlePayment={handlePayment} />
-        </S.Content>
-        <S.Text>
-          <Info size={18} /> Your purchase is protected by a secure connection
-          from the WON platform. By purchasing from our store you agree and
-          agree to our <a href="#">terms of use.</a> After making the purchase
-          you are entitled to a refund within a maximum of 30 days, without any
-          additional cost, as long as the download of the purchased game has not
-          occurred after your purchase.
-        </S.Text>
-        <Divider />
-      </Container>
-
-      <Showcase
-        title={recommendedTitle}
-        games={recommendedGames}
-        highlight={recommendedHighlight}
-      />
-    </Base>
-  )
-}
+    <Showcase
+      title={recommendedTitle}
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+)
 
 export default Cart
